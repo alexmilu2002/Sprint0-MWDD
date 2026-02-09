628443347f27dbca32dd790c6648fa4b2cda650d
alexmilu2002 <amx020csgo@gmail.com>
2026-02-09 12:48:31 +0100

    maandag

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const themeToggle = document.querySelector('.theme-toggle');
+//Light en Dark Mode
+const themeToggle = document.querySelector('.themetoggle');
 
-// Check wat het huidige theme is (of standaard light)
-const currentTheme = localStorage.getItem('theme') || 'light';
-setTheme(currentTheme);
-
+// Check wat het huidige theme is.
 function setTheme(theme) {
   document.documentElement.setAttribute('data-theme', theme);
   localStorage.setItem('theme', theme);
 }
 
+// Eventlistener die reageert wanneer er op de Light/Dark Knop wordt gedrukt
 themeToggle.addEventListener('click', () => {
   const activeTheme = document.documentElement.getAttribute('data-theme');
 
@@ -18,4 +17,16 @@ themeToggle.addEventListener('click', () => {
     setTheme('light');
   }
 });
+
+//Dialog Pop-Up
+const showProfileDialogBtn = document.getElementById("buttonprofile");
+const profileDialog = document.getElementById("profileDialog");
+
+const showGoalsDialogBtn = document.getElementById("buttongoals");
+const goalsDialog = document.getElementById("goalsDialog");
+
+showProfileDialogBtn.addEventListener("click", () => profileDialog.showModal());
+
+showGoalsDialogBtn.addEventListener("click", () => goalsDialog.showModal());
+
 
