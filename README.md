a29d7cbfe2b35a7140980e157a48e3d4202092af
alexmilu2002 <amx020csgo@gmail.com>
2026-02-11 14:50:59 +0100

    ...

--- README.md
+++ README.md
@@ -32,4 +32,30 @@
 <p>Responsiveness. maxwidth en maxheight, viewwidth, clamp, utopia.fyi. object-fit:cover en object-position:.  grid-template-columns:repeat(auto-fit, 10em);. grid-template-area</p>
 
 <h2>Wat ga ik morgen doen?</h2>
-<p>Morgen is een korte dag, maar ik wil in ieder geval een aantal contrast foutjes verbeteren, en een begin maken aan het slinger effect</p
+<p>Morgen is een korte dag, maar ik wil in ieder geval een aantal contrast foutjes verbeteren, en een begin maken aan het slinger effect</p>
+
+<h1>Dag 3 06/06/2026 WEEK1(Afronding)</h1>
+
+<h2>Wat heb ik vandaag gedaan?</h2>
+<p> Vandaag is een korte werkdag, ik was begonnen met mijn code verbeteren. Ik heb een begin gemaakt aan de grid box. Verder heb ik 2 SVG's gemaakt op canva die ik wil gebruiken om mijn informatie te laten zien. En heb ik de buttons in mijn sleutelhanger een unieke vorm gegeven. Toen was het alweer tijd voor de expo. We deden een soort van marktje, waarbij je vrij mocht rondlopen om bij anderen te kijken. Ik vond het leuk om iedereens werk te zien, en ergens motiveerde het mij ook om nog harder bezig te gaan. Ik heb heel veel inspiratie opgedaan om misschien te gebruiken bij een volgend project. Na de expo hebben we nog een quiz gedaan met zijn allen, waarbij je prijzen kon winnen. Ik was heel goed bezig, stond zelfs een tijdje als eerste, alleen raakte ik mijn plekje kwijt. </p>
+<br>
+<p>Deze week heb ik heel veel mogen leren. Ik merk dat we op mijn opleiding weinig tot geen opdrachten krijgen wat met coderen te maken heeft. </p>
+
+<h2>Hoelang duurde het?</h2>
+<p>Workshop's: 2uur</p>
+<p>Workshop Tips toepassen in eigen Site: 2 uur</p>
+<p>Kleuren, Lettertype's en SVG's zoeken: 1 uur</p>
+<p></p>
+
+
+<h2>Wat heb ik geleerd?</h2>
+<p>Theming (Light Mode en Dark Mode), HSL OKLCH, RGB. Accent Color voor de Focus. Font Inherit. </p>
+<p>Responsiveness. maxwidth en maxheight, viewwidth, clamp, utopia.fyi. object-fit:cover en object-position:.  grid-template-columns:repeat(auto-fit, 10em);. grid-template-area</p>
+
+<h2>Wat ga ik morgen doen?</h2>
+<p>Morgen is een korte dag, maar ik wil in ieder geval een aantal contrast foutjes verbeteren, en een begin maken aan het slinger effect</p>
+
+
+<h2> Dag... <h2>
+<h3>To-Do List<h3>
+<p>-Bolletjes laten uitlaten met de cirkel. -API voor mede-studenten fixen. - Read-Me bijhouden. -Als ik tijd overheb misschien een leuke soundeffect toevoegen. -Justus had aanbevolen om met een engine de keychain animatie te doen, zodat je sleutelhangers bewegen als je je telefoon kantelt. Dat iwl ik ook nog doen als ik tijd over heb.</p>
--- script.js
+++ script.js
@@ -1,3 +1,108 @@
+//API gedeelte
+const apiURL = "https://fdnd.directus.app/items/person/279"
+const dialogDestinationPersonal = document.querySelector('.content-destination-personal')
+const dialogDestinationGoals = document.querySelector('.content-destination-goals')
+
+fetchJson(apiURL).then(function (response) {
+  if (dialogDestinationPersonal) {
+    let customField = JSON.parse(response.data.custom);
+
+    dialogDestinationPersonal.innerHTML =
+      `
+        <h2>Wie ben ik?</h2>  
+        <img src="${response.data.avatar}" alt="Avatar van Alex in Zwart-Wit gegenereerd door AI.">
+        <div>
+          <p><strong>Naam:</strong> ${response.data.name ?? "Niet Beschikbaar!"}</p>
+          <p><strong>Bijnaam:</strong> ${response.data.nickname ?? "Niet Beschikbaar!"}</p>
+          <p><strong>Geboortedatum:</strong> ${response.data.birthdate ?? "Niet Beschikbaar!"}</p>
+          <p><strong>Opleiding:</strong> ${customField.opleiding ?? "Niet Beschikbaar!"}</p>
+          <p><strong>Interesses:</strong> ${customField.interesses ?? "Niet Beschikbaar!"}</p> 
+          <p><strong>Hobby's:</strong> ${customField.hobbys ?? "Niet Beschikbaar!"}</p>    
+          <p><strong>Later:</strong> ${customField.later ?? "Niet Beschikbaar!"}</p>   
+        </div>
+        <button>Terug</button>
+      `
+  }
+
+  if (dialogDestinationGoals) {
+    let customField = JSON.parse(response.data.custom);
+
+    dialogDestinationGoals.innerHTML =
+      `
+        <h2>Mijn Leerdoelen</h2>
+        <img src="./images/doelensvg.svg" alt="Foto van een trofee">
+        <div>
+         <p><strong>Leerdoel 1:</strong> ${customField.leerdoel1 ?? "Niet Beschikbaar!"}</p>
+          <p><strong>Leerdoel 2:</strong> ${customField.leerdoel2 ?? "Niet Beschikbaar!"}</p>
+          <p><strong>Leerdoel 3:</strong> ${customField.leerdoel3 ?? "Niet Beschikbaar!"}</p>
+        </div>
+        <button>Terug</button>
+      `
+  }
+})
+
+async function fetchJson(url, payload = {}) {
+  return await fetch(url, payload)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
+
+const apiURL2 ="https://fdnd.directus.app/items/person?filter[squads][squad_id][tribe][name]=CMD%20Minor%20Web%20Dev&filter[squads][squad_id][cohort]=2526";
+const dialogDestinationRandomize = document.querySelector(".content-destination-randomize");
+
+async function haalRandomMinormensOp() {
+  let response = await fetch(apiURL2);
+  let responseJSON = await response.json();
+  let responseData = responseJSON.data;
+
+  let randomNumber = Math.floor(Math.random() * responseData.length);
+  let randomAvatar = responseData[randomNumber].avatar;
+
+  randomNumber = Math.floor(Math.random() * responseData.length);
+  let randomName = responseData[randomNumber].name;
+
+  randomNumber = Math.floor(Math.random() * responseData.length);
+  let randomNickname = responseData[randomNumber].nickname;
+
+  randomNumber = Math.floor(Math.random() * responseData.length);
+  let randomBirthdate = responseData[randomNumber].birthdate;
+
+  randomNumber = Math.floor(Math.random() * responseData.length);
+  let randomFavColor = responseData[randomNumber].fav_color;
+
+  randomNumber = Math.floor(Math.random() * responseData.length);
+  let randomFavSeason = responseData[randomNumber].fav_season;
+
+  randomNumber = Math.floor(Math.random() * responseData.length);
+  let randomFavAnimal = responseData[randomNumber].fav_animal;
+
+  randomNumber = Math.floor(Math.random() * responseData.length);
+  let randomFavHobby = responseData[randomNumber].fav_hobby;
+
+  randomNumber = Math.floor(Math.random() * responseData.length);
+  let randomFavFruit = responseData[randomNumber].fav_fruit;
+
+  // En nu: in je HTML zetten (zoals je docent wil)
+  if (dialogDestinationRandomize) {
+    dialogDestinationRandomize.innerHTML = `
+      <h2>Randomized Student</h2>
+      <img src="${randomAvatar ? randomAvatar : './images/picturenotavailable.jpg' }" alt="Avatar foto van Random gegenereerde Student.">
+      <div>
+        <p><strong>Naam:</strong> ${randomName ?? "Niet Beschikbaar!"}</p>
+        <p><strong>Bijnaam:</strong> ${randomNickname ?? "Niet Beschikbaar!"}</p>
+        <p><strong>Geboortedatum:</strong> ${randomBirthdate ?? "Niet Beschikbaar!"}</p>
+        <p><strong>Favoriete Kleur:</strong> ${randomFavColor ?? "Niet Beschikbaar!"}</p>
+        <p><strong>Favoriete Seizoen:</strong> ${randomFavSeason ?? "Niet Beschikbaar!"}</p>
+        <p><strong>Favoriete Dier:</strong> ${randomFavAnimal ?? "Niet Beschikbaar!"}</p>
+        <p><strong>Favoriete Hobby:</strong> ${randomFavHobby ?? "Niet Beschikbaar!"}</p>
+        <p><strong>Favoriete Fruit:</strong> ${randomFavFruit ?? "Niet Beschikbaar!"}</p>
+      </div>
+      <button>Terug</button>
+    `;
+  }
+  randomizeDialog.showModal();
+}
+
 //Light en Dark Mode
 const themeToggle = document.querySelector('.themetoggle');
 
@@ -19,14 +124,22 @@ themeToggle.addEventListener('click', () => {
 });
 
 //Dialog Pop-Up
-const showProfileDialogBtn = document.getElementById("buttonprofile");
+const showRandomizeDialogBtn = document.querySelector(".randomstudent")
+const randomizeDialog = document.getElementById("randomizeDialog");
+
+const showProfileDialogBtn = document.querySelector(".buttonprofile");
 const profileDialog = document.getElementById("profileDialog");
 
-const showGoalsDialogBtn = document.getElementById("buttongoals");
+const showGoalsDialogBtn = document.querySelector(".buttongoals");
 const goalsDialog = document.getElementById("goalsDialog");
+
+showRandomizeDialogBtn.addEventListener("click", function() {
+  haalRandomMinormensOp();
+  });
 
 showProfileDialogBtn.addEventListener("click", () => profileDialog.showModal());
 
 showGoalsDialogBtn.addEventListener("click", () => goalsDialog.showModal());
+
 
 
